ba3d6675d3fae5ea0d32215fbdfea3c7961bd7ca
Gabriel Redig <glredig@gmail.com>
2015-04-02 16:51:01 -0700

    increase word count

--- scripts/tyyp.js
+++ scripts/tyyp.js
@@ -59,7 +59,7 @@ var TYYP = {
     
     var deferred = Q.defer(),
         req      = new XMLHttpRequest();
-        url      = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=6&limit=30&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        url      = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=20&limit=30&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
     
     req.open('GET', url, true);
 
